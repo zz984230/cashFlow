e2e1a6b6c0716e2630a8c6e32c7c1ffaae889ffb
zhao <mephisto512@aliyun.com>
2020-11-15 18:53:48 +0800

    unit test

--- internal/config/config.py
+++ internal/config/config.py
@@ -1,43 +1,38 @@
-from abc import abstractmethod, ABCMeta
 import toml
 import os
 
 
-class ConfigHandler(metaclass=ABCMeta):
-    @abstractmethod
-    def set_cfg_path(self, cfg_path):
-        pass
-
-    @abstractmethod
-    def update(self):
-        pass
-
-    @abstractmethod
-    def db_cfg(self):
-        pass
-
-
-class Config(ConfigHandler):
+class Config(object):
     def __init__(self):
-        self.cfg_path = ""
-        self.db_cfg = None
-        self.cfg = {}
+        self.__cfg_path = ""
+        self.__db_cfg = None
+        self.__cfg = {}
 
     def set_cfg_path(self, cfg_path):
-        self.cfg_path = cfg_path
+        self.__cfg_path = cfg_path
 
     def __decode(self):
-        if not os.path.exists(self.cfg_path):
-            raise Exception("f{self.cfg_path} dose not exist")
+        if not os.path.exists(self.__cfg_path):
+            raise Exception("%s does not exist" % self.__cfg_path)
 
-        toml.load(self.cfg_path, self.cfg)
+        with open(self.__cfg_path, "r") as f:
+            self.__cfg = toml.load(f)
+        self.__db_cfg = self.__cfg["database"]
 
     def update(self):
         self.__decode()
 
+    @property
     def db_cfg(self):
-        return self.db_cfg
+        return self.__db_cfg
+
+    @db_cfg.setter
+    def db_cfg(self, db_cfg):
+        self.__db_cfg = db_cfg
 
 
 if __name__ == "__main__":
-    c = ConfigHandler("")
+    c = Config()
+    c.set_cfg_path("./conf.toml")
+    c.update()
+    print(c.db_cfg)
